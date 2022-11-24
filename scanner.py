3bef7706b435ac72003cd65113d8f7c527925817
erfan <erfan@smartech.ir>
2022-11-25 03:10:41 +0330

    update State class
    Co-authored-by: AmirMohammadFakhimi <AmirMohammadFakhimi@users.noreply.github.com>

--- scanner.py
+++ scanner.py
@@ -1,27 +1,47 @@
 buffer = ""
 begin_pointer = 0
 forward_pointer = 0
-states = []
 start_state = None
 number_of_line = 1
 
 letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-numbers = "0123456789"
+digit = "0123456789"
 whitespace = "\n\f\r\v\t" + chr(32)
 symbols = ";:,[](){}+-*=</"
+other = "other"
 
 
 class State:
+    states = []
+    _number = 0
     _destination_states = dict()
 
-    def __init__(self, number):
-        self.number = number
+    def __init__(self, should_back=False, token=None):
+        self.number = State._number
+        self.should_back = should_back
+        self.token = token
+        State.states.append(self)
+        State._number += 1
 
-    def add_dest_state(self, input_letters, destination_state):
-        for letter in input_letters:
-            self._destination_states[letter] = destination_state
+    def is_final(self):
+        return self.token != None
+
+    def add_all_states(self, states: dict, not_split_keys=None):
+        if not_split_keys is None:
+            not_split_keys = ["other"]
+        for key, value in states:
+            self.add_dest_state(key, value, key not in not_split_keys)
+
+    def add_dest_state(self, input_letters, destination_state, should_split=True):
+        if should_split:
+            for letter in input_letters:
+                self._destination_states[letter] = destination_state
+        else:
+            self._destination_states[input_letters] = destination_state
 
     def move(self, input_letter):
+        if input_letter not in self._destination_states.keys():
+            return self._destination_states["other"]
         return self._destination_states[input_letter]
 
 
@@ -32,9 +52,28 @@ def initial_input_file():
 
 
 def initial_DFA():
-    global states, start_state
-    start_state = State(0)
-    start_state.add_dest_state("")
+    global start_state
+    start_state = State()
+
+    # digit
+    digit_state1 = State()
+    digit_state2 = State(True, "NUMBER")
+    digit_state1.add_all_states({digit: digit_state1, other: digit_state2})
+
+    # letter
+    digit_state1 = State()
+    digit_state2 = State(True, "NUMBER")
+    digit_state1.add_all_states({digit: digit_state1, other: digit_state2})
+
+
+    # comment
+    comment_state1 = State()
+    comment_state2 = State(True, "SYMBOL")
+    comment_state3 = State()
+    comment_state4 = State()
+    comment_state5 = State()
+    comment_state6 = State(True, "COMMENT")
+    # TODO add all start state
 
 
 def get_next_token():
