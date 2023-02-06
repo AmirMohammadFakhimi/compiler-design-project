a9658a6e90e305af1b0af80f72e48bcea627a77b
Amir Mohammad Fakhimi <fakhimi.amirmohamad@gmail.com>
2023-02-07 03:29:15 +0330

    test_code_gen.py bugs fixed Co-authored-by: Erfan Sadraiye <erfansadraiye@users.noreply.github.com>

--- test_code_gen.py
+++ test_code_gen.py
@@ -1,22 +1,30 @@
 import os
+import subprocess
 
+# just for LINUX
 if __name__ == '__main__':
-    test_directories = [x[0] for x in os.walk('testcases')]
+    # test_directories = os.listdir('testcases')
+    test_directories = []
+    for i in range(1, 10):
+        test_directories.append(f'T{i}')
 
     for directory_name in test_directories:
-        os.chdir(directory_name)
-        os.system('python3 ../compiler.py')
-        os.chdir('../..')
+        if directory_name.startswith('.'):
+            continue
 
-        input_file = open(f'{directory_name}/input.txt', 'r')
-        expected_output_file = open(f'{directory_name}/output.txt', 'r')
+        input_file = open(f'testcases/{directory_name}/input.txt', 'r')
+        expected_otestutput_file = open(f'testcases/{directory_name}/output.txt', 'r')
+        expected_output = expected_output_file.read()
+        expected_output_file.close()
 
         current_dir_input = open(f'input.txt', 'w')
         current_dir_input.write(input_file.read())
-        os.system('./tester_linux.out')
-        output_file = open(f'output.txt', 'r')
+        current_dir_input.close()
+        input_file.close()
 
-        if output_file.read() == expected_output_file.read():
+        output = subprocess.Popen('./tester_linux.out', stdout=subprocess.PIPE).communicate()[0]
+
+        if output == expected_output:
             print(f'{directory_name} passed.')
         else:
             print(f'{directory_name} failed.')
