a7e39eb45133034b421453f4e65c42b7d447c56f
erfan <erfan@smartech.ir>
2023-02-06 23:11:05 +0330

    fix a minor bug in grammar

--- code_gen.py
+++ code_gen.py
@@ -36,117 +36,116 @@ def action_routine(symbol_action):
     global i
     current_input = parser.top_token[1]
     symbol_action = int(symbol_action)
-    if symbol_action == 66:  # pid
+    if symbol_action == 64:  # pid
         p = getaddr(current_input)
         ss.append(p)
 
-    elif symbol_action == 67:  # pnum
+    elif symbol_action == 65:  # pnum
         ss.append(f'#{current_input}')
 
-    elif symbol_action == 43:
+    elif symbol_action == 42:
         pb.append(generate_code("ASSIGN", ss[-1], ss[-2], ))
         i += 1
         pop_ss(1)
 
-    elif symbol_action == 50:  # add
+    elif symbol_action == 49:  # add
         t = gettemp()
         pb.append(generate_code("ADD", ss[-1], ss[-2], t))
         i += 1
         pop_ss(2)
         ss.append(t)
 
-    elif symbol_action == 52:  # sub
+    elif symbol_action == 51:  # sub
         t = gettemp()
         pb.append(generate_code("SUB", ss[-1], ss[-2], t))
         i += 1
         pop_ss(2)
         ss.append(t)
 
-    elif symbol_action == 53:  # mul
+    elif symbol_action == 52:  # mul
         t = gettemp()
         pb.append(generate_code("MULT", ss[-1], ss[-2], t))
         i += 1
         pop_ss(2)
         ss.append(t)
 
-    elif symbol_action == 55:  # div
+    elif symbol_action == 54:  # div
         t = gettemp()
         pb.append(generate_code("DIV", ss[-2], ss[-1], t))
         i += 1
         pop_ss(2)
         ss.append(t)
 
-    elif symbol_action == 72:  # save
+    elif symbol_action == 70:  # save
         ss.append(i)
         pb.append('')
         i += 1
 
-    elif symbol_action == 68:  # jpf_save
+    elif symbol_action == 66:  # jpf_save
         pb[ss[-1]] = generate_code("JPF", ss[-2], str(i + 1))
         pop_ss(2)
         ss.append(i)
         pb.append('')
         i += 1
 
-    elif symbol_action == 69:  # label
+    elif symbol_action == 67:  # label
         ss.append(i)
 
-
-    elif symbol_action == 33:  # jp
+    elif symbol_action == 32:  # jp
         pb[ss[-1]] = generate_code("JP", i)
         ss.pop()
 
-    elif symbol_action == 32:  # jpf
+    elif symbol_action == 31:  # jpf
         pb[ss[-1]] = generate_code("JPF", ss[-2], str(i))
         pop_ss(2)
 
-    elif symbol_action == 34:  # while
+    elif symbol_action == 33:  # while
         pb[ss[-1]] = generate_code("JPF", ss[-2], str(i + 1))
         pb.append(generate_code("JP", ss[-3]))
         i += 1
         pop_ss(4)
 
-    elif symbol_action == 49:  # equal
+    elif symbol_action == 48:  # equal
         t = gettemp()
         pb.append(generate_code("EQ", ss[-1], ss[-2], t))
         i += 1
         pop_ss(2)
         ss.append(t)
 
-    elif symbol_action == 47:  # lt
+    elif symbol_action == 46:  # lt
         t = gettemp()
         pb.append(generate_code("LT", ss[-2], ss[-1], t))
         i += 1
         pop_ss(2)
         ss.append(t)
 
-    elif symbol_action == 46:  # get_value
+    elif symbol_action == 45:  # get_value
         addr = ss[-2] + ss[-1]
         ss.append(addr)
 
-    elif symbol_action == 29:  # pop
+    elif symbol_action == 28:  # pop
         ss.pop()
 
-    elif symbol_action == 30:  # break action
+    elif symbol_action == 29:  # break action
         pb.append(generate_code("JP", ss[-4]))
         i += 1
 
-    elif symbol_action == 71:  # switch_compare
+    elif symbol_action == 69:  # switch_compare
         temp = gettemp()
         pb.append(generate_code('EQ', ss[-2], ss[-1], temp))
         i += 1
         pop_ss(1)
         ss.append(temp)
 
-    elif symbol_action == 37:
+    elif symbol_action == 36: # switch end
         pop_ss(1)  # pops expression
         pb[ss[-1]] = generate_code("JP", i)
         pop_ss(1)  # pops i
 
-    elif symbol_action == 70:  # jp_forward
+    elif symbol_action == 68:  # jp_forward
         pb.append(generate_code("JP", i + 1))
         i = i + 1
 
-    elif symbol_action == 65:
+    elif symbol_action == 59:
         pb.append(generate_code("PRINT", ss[-1]))
         i = i + 1
